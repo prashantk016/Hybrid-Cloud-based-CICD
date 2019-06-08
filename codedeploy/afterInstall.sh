1172b990678d2c1424cd0c6b06c8ee5e64d5b2f4
Prashant K <kabra.p@husky.neu.edu>
2019-06-08 16:16:51 -0400

    updated code deploy

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
-
-sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
-
-# cleanup log files
-sudo rm -rf /opt/tomcat/logs/catalina*
-sudo rm -rf /opt/tomcat/logs/*.log
-sudo rm -rf /opt/tomcat/logs/*.txt
-
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/opt/cloudwatch-config.json \
-    -s
+sudo rm -rf /webapp
+ls -a
+echo "files inside app folder"
+ls /app -a
+sudo unzip app/s.zip /webapp
+echo "files inside webapp folder"
+ls /webapp -a
+cd webapp/Homeworks.dll
