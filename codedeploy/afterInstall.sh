b5c60732c670e8ce1f9506ccf621df64445ae171
Prashant K <kabra.p@husky.neu.edu>
2019-06-09 16:16:49 -0400

    updated

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -4,7 +4,7 @@ sudo rm -rf /webapp
 ls -a
 echo "files inside app folder"
 ls /app -a
-sudo unzip app/s.zip /webapp
+sudo unzip /app/s.zip /webapp   
 echo "files inside webapp folder"
 ls /webapp -a
-dotnet webapp/Homeworks.dll
+dotnet /webapp/Homeworks.dll
