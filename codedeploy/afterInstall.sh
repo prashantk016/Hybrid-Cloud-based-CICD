a61b361b76161b9881cdc0bac69890eaa4567257
Prashant K <kabra.p@husky.neu.edu>
2019-06-09 16:42:24 -0400

    Revert "testing with dotnet run"
    This reverts commit f377ce1a5ccbd6167ae2a704f302504d52fcc02e.

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-dotnet run
+
+sudo rm -rf /webapp
+ls -a
+echo "files inside app folder"
+ls /app -a
+sudo unzip /app/s.zip /webapp   
+echo "files inside webapp folder"
+ls /webapp -a
+dotnet /webapp/Homeworks.dll
