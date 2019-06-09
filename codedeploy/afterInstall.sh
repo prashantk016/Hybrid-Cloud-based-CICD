f086c9b6d6650aa42fa4fff2328c2e8a7841f881
Prashant K <kabra.p@husky.neu.edu>
2019-06-09 17:44:21 -0400

    updated afterinstall and index

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+kill $(ps aux | grep 'Homeworks.dll' | awk '{print $2}')
+echo "---Webapp stopped---"
 sudo rm -rf /webapp
 ls -a
-echo "files inside app folder"
+echo "---files inside app folder---"
 ls /app -a
 sudo unzip -o /app/s.zip -d /webapp   
-echo "files inside webapp folder"
+echo "---files inside webapp folder---"
 ls /webapp -a
-dotnet /webapp/Homeworks.dll urls="http://*:5000"
+nohup dotnet /webapp/Homeworks.dll urls="http://*:5000" &
+echo "---Webapp started---"
