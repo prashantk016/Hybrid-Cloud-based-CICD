f377ce1a5ccbd6167ae2a704f302504d52fcc02e
Prashant K <kabra.p@husky.neu.edu>
2019-06-09 16:42:24 -0400

    testing with dotnet run

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-sudo rm -rf /webapp
-ls -a
-echo "files inside app folder"
-ls /app -a
-sudo unzip /app/s.zip /webapp   
-echo "files inside webapp folder"
-ls /webapp -a
-dotnet /webapp/Homeworks.dll
+dotnet run
